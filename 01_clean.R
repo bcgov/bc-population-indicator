b69961e3e1c5c545a1749086b29c49611cfc3a34
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2017-12-28 16:41:55 -0800

    update data; use sf

--- 01_clean.R
+++ 01_clean.R
@@ -10,63 +10,90 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
+library(readr) #data import
 library(dplyr) #data munging
-library(tidyr) #for reformatting dataframes
-library(reshape2) #format dataframe
-library(rgdal) #for reading shapefile
-library(rgeos) #for polygon area
-library(envreportutils) #for order dataframe function
-library(bcmaps) #for regional district map plot; package & details on GitHub -- https://github.com/bcgov/bcmaps
+library(bcmaps) #for BC regional district map
+library(sf) #sf map object 
+library(stringr) #modifying character strings
+# library(tidyr) #for reformatting dataframes
+# library(reshape2) #format dataframe
+# library(rgdal) #for reading shapefile
+# library(rgeos) #for polygon area
+
 
 ## Tabular data files publically available from BC Stats on-line: 
-## http://www.bcstats.gov.bc.ca/StatisticsBySubject/Demography/PopulationEstimates.aspx
-## [license: BC Crown Copyright]
-
-## loading tabular annual population data for BC from 1867-2015
-## downloaded directly from BC Stats and cleaned for machine-readable format
-popn <- read.csv("~/soe_pickaxe/Operations ORCS/Data - Working/sustainability/population/Population_Estimates.csv", stringsAsFactors = FALSE)
-
-## loading tabular regional district population data from 1986-2015
-## downloaded via search tool under Population by Age and Sex, first cell renamed to SGC for machine readable format
-popn_bc <- read.csv("~/soe_pickaxe/Operations ORCS/Data - Working/sustainability/population/BC_annual_population_estimates.csv", stringsAsFactors = FALSE)
-
-
-## preparing regional district map for census division population display and area
-cd <- regional_districts_disp
-cd_area <- regional_districts_analysis
-
-## note for next update -- use regional_districts_analysis for area calculation 
-## extract regional district area values
-area_vector <- gArea(cd, byid = TRUE)
-area_df <- data.frame(SGC = cd_area$CDUID, area = area_vector)
-
-## format population values in BC dataframe
-popn_bc <- popn_bc %>% 
+## https://www2.gov.bc.ca/gov/content?id=36D1A7A4BEE248598281824C13CB65B6
+## [licence: B.C. Crown Copyright]
+## annual population data for BC from 1867-2017 downloaded directly from BC Stats 
+bcregpopdata <- "~/soe_pickaxe/Operations ORCS/Data - Working/sustainability/population/2018/Population_Estimates.csv"
+## regional district population data from 1986-2016 downloaded via search tool here: https://www.bcstats.gov.bc.ca/apps/PopulationEstimates.aspx
+bcpopdata <- "~/soe_pickaxe/Operations ORCS/Data - Working/sustainability/population/2018/BC annual population estimates.csv"
+  
+## read in and clean BC population CSV file
+popn_bc <- read_csv(bcpopdata, skip = 2, n_max = 155, col_types = "cn") %>% 
+  rename(Population = `Population: June 1`) %>% 
+  na.omit() %>% 
+  filter("Year" != Year) %>% 
   mutate(popn_million = round(Population/1000000, 2))
 
-## calculate BC population change from 1986 - 2015
-bc_change <- popn_bc$Population[popn_bc$Year == 2015] - popn_bc$Population[popn_bc$Year == 1986]
-bc_pct <- bc_change/popn_bc$Population[popn_bc$Year == 1986]*100
+## read in and clean BC population by Regional District CSV file
+popn <- read_csv(bcregpopdata) %>% 
+  select(- Gender) %>% 
+  rename(SGC = X1) %>% 
+  filter(`Regional District` != "British Columbia") %>% 
+  mutate(popn_thousand = round(Total/1000, 0)) %>% 
+  rename(Regional_District = `Regional District`) %>% 
+  mutate(Regional_District = str_replace(Regional_District, "-", " - ")) 
 
-## clean regional district dataframe 
-popn_rd <- popn %>% 
-  filter(Regional.District != "British Columbia") %>% 
-  dplyr::select(SGC, Regional.District, Year, Total) %>% 
-  mutate(popn_thousand = round(Total/1000, 0))
-  
-## format dash signs in regional district dataframe
-popn_rd$Regional.District <- gsub("-", " - ", popn_rd$Regional.District)
+## 2016 Population by RD
+popn_sum <- popn %>% 
+  group_by(Regional_District) %>% 
+  filter(Year == 2016)
 
-## format SGC code to match with CDUID code in shapefile for merging dataframes later
-for (i in 1:length(popn_rd$SGC)) {
-  if (nchar(popn_rd$SGC[i]) == 4) {
-    popn_rd$SGC[i] <- sub("^", "590", popn_rd$SGC[i])
-  }
-  else {
-    popn_rd$SGC[i] <- sub("^", "59", popn_rd$SGC[i])
-  }
-  popn_rd$SGC[i] <- substr(popn_rd$SGC[i], 0, 4)
-}
+## df to separate Greater Vancouver from other RD with smaller population sizes
+popn_gv <- popn_sum %>% 
+  filter(Regional_District == "Greater Vancouver")
+
+popn_rest <- popn_sum %>% 
+  filter(Regional_District != "Greater Vancouver")
+
+## Combine RDs with Northern Rockies Regional Municipality
+mun <- get_layer("municipalities") %>% 
+  filter(ADMIN_AREA_ABBREVIATION == "NRRM") %>% 
+  select(ADMIN_AREA_TYPE, ADMIN_AREA_NAME, SHAPE_Area, SHAPE)
+
+rd <- get_layer("regional_districts") %>% 
+  select(ADMIN_AREA_TYPE, ADMIN_AREA_NAME, SHAPE_Area, SHAPE) %>% 
+  rbind(mun) %>% 
+  mutate(ADMIN_AREA_NAME = str_replace(ADMIN_AREA_NAME, "-", " - ")) %>% 
+  mutate(ADMIN_AREA_NAME = str_replace(ADMIN_AREA_NAME, c("Regional District of |Regional District| Regional Municipality|Unincorporated"), "")) %>% 
+  mutate(ADMIN_AREA_NAME = str_trim(ADMIN_AREA_NAME, side = "right"))
+                
+## 2016 Population Density
+## extract regional district area values
+area_vector <-  rd$SHAPE_Area 
+area_df <- data.frame(Regional_District = rd$ADMIN_AREA_NAME, area = area_vector) 
+
+diff1 <- setdiff(popn_sum$Regional_District, area_df$Regional_District)
+diff2 <- setdiff(area_df$Regional_District, popn_sum$Regional_District)
+
+
+%>% 
+  left_join(popn_sum, by = c("Regional_District" = "Regional_District"))
+
+## Need to fix names to match other df 
+
+## join popn_sum and regional district area dfs to calculate population density
+popn_sum <- 
+popn_sum <- popn_sum %>%
+  mutate(density = round(Total/(area/10^6), 0))
+
+## create density labels and categories for plotting density map
+catlab <- c("less than 10", "10 to 60", "61 to 200", "greater than 800")
+popn_sum$cat <- cut(popn_sum$density, breaks = c(-1,10,60,200,900),
+                    include.lowest=TRUE,
+                    labels = catlab)
+
 
 
 ## calculate total change in population from 1986 to 2015 in regional districts
@@ -86,27 +113,13 @@ popn_sum <- popn_rd %>%
   filter(Year != 1986)
 
 
-## creating new dataframe to separate Greater Vancouver from other rd with less population
-popn_gv <- popn_sum %>% 
-  filter(Regional.District == "Greater Vancouver")
 
-popn_rest <- popn_sum %>% 
-  filter(Regional.District != "Greater Vancouver")
 
 ## ordering other regionals districts dataframe based on 2015 population
 #popn_rest <- popn_rest[order(popn_rest$popn_thousand, decreasing = TRUE), ]
 popn_rest <- order_df(popn_rest, target_col = "Regional.District",
                       value_col = "popn_thousand", fun = sum, desc = TRUE)
 
-## join popn_sum and regional district area dataframes to calculate population density
-popn_sum <- left_join(popn_sum, area_df, by = c("SGC" = "SGC"))
-popn_sum <- popn_sum %>%
-  mutate(density = round(Total/(area/10^6), 0))
 
-## create density labels and categories for plotting density map
-catlab <- c("less than 10", "10 to 60", "61 to 200", "greater than 800")
-popn_sum$cat <- cut(popn_sum$density, breaks = c(-1,10,60,200,900),
-                    include.lowest=TRUE,
-                    labels = catlab)
 
 
